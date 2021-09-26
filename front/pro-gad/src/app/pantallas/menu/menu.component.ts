f1f16e67f9bfc6ad52b644d002a4eda8be2572ef
KH0RE <cet.quinaluisa@yavirac.edu.ec>
2021-09-25 23:32:26 -0500

    Pantalla contactos / Memu y Footer Terminados

--- front/pro-gad/src/app/pantallas/menu/menu.component.ts
+++ front/pro-gad/src/app/pantallas/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router : Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  home(){
+    this.router.navigate(['gad/'])
+  }
+  autoridades(){
+    this.router.navigate(['gad/autoridades'])
+  }
+  historia(){
+    this.router.navigate(['gad/historia'])
+  }
+  turismo(){
+    this.router.navigate(['gad/turismo'])
+  }
+  contactos(){
+    this.router.navigate(['gad/contactos'])
+  }
+  vision(){
+    this.router.navigate(['gad/quienes-somos'])
   }
 
 }
