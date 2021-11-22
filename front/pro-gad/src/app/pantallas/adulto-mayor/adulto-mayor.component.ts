83c76f2c2c4e7f5cafa973b129fded431fb627d6
joel593 <drouet593@gmail.com>
2021-11-21 21:21:25 -0500

    actualizaciond

--- front/pro-gad/src/app/pantallas/adulto-mayor/adulto-mayor.component.ts
+++ front/pro-gad/src/app/pantallas/adulto-mayor/adulto-mayor.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-adulto-mayor',
   templateUrl: './adulto-mayor.component.html',
-  styleUrls: ['./adulto-mayor.component.css']
+  //styleUrls: ['./adulto-mayor.component.css']
 })
 export class AdultoMayorComponent implements OnInit {
 
